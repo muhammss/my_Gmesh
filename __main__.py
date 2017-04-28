c4d8c702b1b171f5b6c8a6949c1ad6263b8e2dee
Salman <muhammad.siddiqui@math.ntnu.no>
2017-04-28 12:05:57 +0200

    new

--- __main__.py
+++ __main__.py
@@ -5,4 +5,16 @@ import importlib
 from os.path import dirname, isfile, split, splitext
 import sys
 import textwrap
-m
+
+@click.group()
+def main():
+	pass
+
+@main.command()
+@click.option('--out', type=str, required=False)
+@click.option('--normal', type=float, nargs=3, required=False)
+@click.option('--base', type=float, nargs3, required=False)
+@click.option('--auto/--no-auto', default=False)
+@@click.argument('filenames', type=str, nargs=1)
+
+@main.command()
